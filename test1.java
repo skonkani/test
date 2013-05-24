ee782434330094a926d5f8d52cbf4c95f36017a9
skonkani <skonkani@oakland.edu>
2013-05-24 11:48:00 -0400

    testing git

---
+++ test1.java
@@ -0,0 +1,27 @@
+public class test1
+{
+
+	int n1;
+	int n2;
+
+	public void setn1(int num1)
+	{
+		n1=num1;
+
+	}
+	public void setn2(int num1)
+	{
+		n2=num1;
+
+	}
+	public int getn1()
+	{
+		return n1;
+	}
+
+	public int getn2()
+	{
+		return n2;
+	}
+
+}
